414abebfcc2e2a04784674ed79b3148d884abbd7
krushna852 <krushnawalunjkar11@gmail.com>
2026-08-26 18:14:31 +0530

    Series and Operation on It

---
+++ Series.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+s1=pd.Series([10,20,30,40,50])  # it is 1D data structure and it is homogenous (only one data type)
+print(s1)
+
+print(s1.dtype)
+
+
+s2=pd.Series([10,63, 45,2.3])
+print(s2,"\n",s2.dtype)
+
+s3=pd.Series(["krushna",56.2,56,1])
+print(s3,"\n",s3.dtype)
+
+print(s3.values)
+
+print(s1.index)
+
+s3.name="Random"
+print(s3)
